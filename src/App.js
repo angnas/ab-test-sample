413f4ed008a9b99c5b6f8002da3cecd1e329807d
Angela Naseri <f.naseri1993@gmail.com>
2024-02-21 14:58:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+import Body1 from './components/Body1';
+import Body2 from './components/Body2';
+
+
+
 function App() {
+  const [visitor, setVisitor] = useState(0);
+
+  useEffect(() => {
+    const randomizeVisitor = Math.random();
+    if (randomizeVisitor > 0.5) {
+      setVisitor(1);
+    }
+
+    }, []);
+
+
+
+  const handleClick = () => {
+    console.log ({
+      event: "ctaClicked" ,
+      cta : "click" ,
+      variant : visitor === 0 ? "control" : "variantB",
+    });
+    window.location.pathname = "?variant=variantB";
+
+  }
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {visitor === 0 ? (
+        <>
+        <Body1 />
+        <div className='button-container'>
+        <button className="button" onClick={handleClick}>Try More Recipes</button>
+
+        </div>
+        
+  
+        </>
+        
+      ) : (
+        <>
+        <Body2 />
+        <div className='button-container'>
+        <button className="button" onClick={handleClick}>Try More Recipes</button>
+
+        </div>
+       
+
+        </>
+        
+      )}
+      
+
     </div>
+    
+     
+    
   );
 }
 
---
+++ src/components/Body2.js
@@ -0,0 +1,38 @@
+import pizza from './pizza.png';
+
+export default function Body2() {
+    return (
+
+ <div className='container'>
+      <img src={pizza} alt="Pizza" />
+
+      <div className='description'>
+      <h3>Ingredients</h3>
+      <p>1 TBSP + 1 tsp olive oil, divided</p>
+      <p>2 cups almond flour</p>
+      <p>2 eggs</p>
+      <p>½ cup dry curd cottage cheese</p>
+      <p>¼ tsp salt</p>
+      <p>2 cups Mom's Tomato Sauce (see Related Recipes)</p>
+      <p>1 tsp oregano</p>
+      <p>¼-¾ cup grated cheddar cheese</p>
+
+<h3>Instructions</h3>
+<ul>
+  <li>Preheat the oven to 350°F (180°C, or gas mark 4).</li>
+  <li>Grease a 9x13-inch (23x33-cm) baking dish or round pizza pan with the tsp of oil and set aside.</li>
+  <li>To make the crust, in a bowl, combine the remaining TBSP (15 ml) olive oil with the almond flour, eggs, dry curd cottage cheese, and salt.</li>
+  <li>Roll it into a ball, transfer it to the greased baking dish, a pat it down into a thin layer.</li>
+  <li>Bake in the oven for 20 minutes or until golden brown.</li>
+  <li>Remove from the oven and top with tomato sauce, oregano, and cheddar cheese.</li>
+  <li>Place the dish back in the oven and bake for another 5 minutes, or until the cheese is melted.</li>
+  
+</ul>
+
+      </div>
+
+
+    </div>
+
+    )
+}
